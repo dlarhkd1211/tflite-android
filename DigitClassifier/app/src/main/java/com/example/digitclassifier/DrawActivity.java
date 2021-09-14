0321548e4573371ec87b916a637d84e6ec4f79c6
Eunkyu-Choi <dlarhkd1211@naver.com>
2021-09-14 22:44:31 +0900

    add activity Button

--- DigitClassifier/app/src/main/java/com/example/digitclassifier/DrawActivity.java
+++ DigitClassifier/app/src/main/java/com/example/digitclassifier/DrawActivity.java
@@ -1,8 +1,23 @@
-package com.example.digitclassifier;
+
+package com.example.digitclassifier.draw;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.divyanshu.draw.widget.DrawView;
+import com.example.digitclassifier.R;
+import com.example.digitclassifier.tflite.*;
+
+import java.io.IOException;
+import java.util.Locale;
+
 
 public class DrawActivity extends AppCompatActivity {
 
@@ -10,5 +25,20 @@ public class DrawActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw);
+
+        DrawView drawView = findViewById(R.id.drawView);
+        drawView.setStrokeWidth(100.0f);
+        drawView.setBackgroundColor(Color.BLACK);
+        drawView.setColor(Color.WHITE);
+
+        Button classifyBtn = findViewById(R.id.classifyBtn);
+        classifyBtn.setOnClickListener(v -> {
+            Bitmap image = drawView.getBitmap();
+        });
+
+        Button clearBtn = findViewById(R.id.clearBtn);
+        clearBtn.setOnClickListener(v -> {
+            drawView.clearCanvas();
+        });
     }
 }
